7ba4cc0757988db3fcb84666c437d33d37a6fd41
javedoct24 <javedoct24@gmail.com>
2024-12-24 19:42:16 +0530

    Add test cases to perform for single number and two number in string

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 RSpec.describe StringCalculator do
-  
+  describe "add number calculator" do
+
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it "returns a single number" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+  end
 end
